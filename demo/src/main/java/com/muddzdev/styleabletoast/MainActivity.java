32629f5c69f9e05e3635342d65844bf80f03c8a2
Muddz <aux-cc@hotmail.com>
2018-03-11 10:42:14 +0100

    - New demo init

--- demo/src/main/java/com/muddzdev/styleabletoast/MainActivity.java
+++ demo/src/main/java/com/muddzdev/styleabletoast/MainActivity.java
@@ -1,40 +1,50 @@
 package com.muddzdev.styleabletoast;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatSeekBar;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.b1)
+    Button b1;
+    @BindView(R.id.b2)
+    Button b2;
+    @BindView(R.id.b3)
+    Button b3;
+    @BindView(R.id.b4)
+    Button b4;
+    @BindView(R.id.b5)
+    Button b5;
+    @BindView(R.id.b6)
+    Button b6;
+    @BindView(R.id.b7)
+    Button b7;
+    @BindView(R.id.b8)
+    Button b8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
 
+    }
 }
